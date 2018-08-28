4d0e1151bbb8ac74658aa2219d53902d7396b8cb
JakeComing <jake@d4webdesign.com>
2018-08-27 21:13:33 -0700

    Round output coordinates to 2 decimals and output as multi-dimensional array

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = function(width, type) {
   console.log("type is:" + type);
   var coordinates = new Array();
-  height = (width * Math.sqrt(3)) / 2;
-  coordinates[0] = ( 0.5 * width ) +","+0;
-  coordinates[1] = width + "," + ( 0.25 * height);
-  coordinates[2] = width + "," + ( 0.75 * height);
-  coordinates[3] = ( 0.5 * width ) +","+ height;
-  coordinates[4] = 0 + "," + ( 0.75 * height);
-  coordinates[5] = 0 + "," + ( 0.25 * height);
+  var height = Math.round((width * Math.sqrt(3)) * 50) / 100;
+  coordinates[0] = [Math.round( 50 * width ) / 100 , 0];
+  coordinates[1] = [width , Math.round( 25 * height ) / 100];
+  coordinates[2] = [width , Math.round( 75 * height) / 100];
+  coordinates[3] = [Math.round( 50 * width ) / 100, height];
+  coordinates[4] = [0 , Math.round( 75 * height) / 100];
+  coordinates[5] = [0 , Math.round( 25 * height) / 100];
   return coordinates;
 };
