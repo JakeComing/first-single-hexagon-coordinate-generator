192dd9f88ef7d9890f656fdc839037f8eb6f9438
JakeComing <jake@d4webdesign.com>
2018-08-28 18:31:59 -0700

    Add pointyTopBool param and conditional

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-module.exports = function(width, type) {
-  console.log("type is:" + type);
+module.exports = function(width, pointyTopBool) {
   var coordinates = new Array();
-  var height = Math.round((width * Math.sqrt(3)) * 50) / 100;
-  coordinates[0] = [Math.round( 50 * width ) / 100 , 0];
-  coordinates[1] = [width , Math.round( 25 * height ) / 100];
-  coordinates[2] = [width , Math.round( 75 * height) / 100];
-  coordinates[3] = [Math.round( 50 * width ) / 100, height];
-  coordinates[4] = [0 , Math.round( 75 * height) / 100];
-  coordinates[5] = [0 , Math.round( 25 * height) / 100];
-  return coordinates;
+  if (pointyTopBool) {
+    var height = Math.round((width * Math.sqrt(3)) * 50) / 100;
+    coordinates[0] = [Math.round( 50 * width ) / 100 , 0];
+    coordinates[1] = [width , Math.round( 25 * height ) / 100];
+    coordinates[2] = [width , Math.round( 75 * height) / 100];
+    coordinates[3] = [Math.round( 50 * width ) / 100, height];
+    coordinates[4] = [0 , Math.round( 75 * height) / 100];
+    coordinates[5] = [0 , Math.round( 25 * height) / 100];
+    return coordinates;
+  } else if (!pointyTopBool) {
+    // flat top calculations
+  }
 };
